5b24b499f4d4cd33f0e1d3eaa832964a3af0bf09
Abdeali <abdealichharchhoda786@gmail.com>
2022-08-04 18:44:14 +0530

    Basic CSS done -Player Turn -Button Css

--- Script.js
+++ Script.js
@@ -7,16 +7,26 @@ let audio_Music = new Audio("./External/music.mp3");
 let audio_ting = new Audio("./External/ting.mp3");
 let audio_gameover = new Audio("./External/gameover.mp3");
 
-let Turn = "X";
-let isGameOver=false;
+let Player=document.getElementById('playername');
+let Turn = document.getElementById('playername').innerText;
+let isGameOver = false;
+
+
+console.log(document.getElementById('playername').innerText);
+console.log(document.getElementById('playername').innerHTML);
+console.log(Turn);
+
 
 // Change Turn Function //
 
-const ChangeTurn = () => {
-
+const ChangeTurn = () => 
+{
     return Turn === "X" ? "0" : "X";
     // condition ? true : false
+
 };
+
+
 
 // Function To Check for a Win 
 
@@ -30,20 +40,20 @@ const ChangeTurn = () => {
 
 const checkWin = () => {
 
-    let boxtext=document.getElementsByClassName('boxtext');
+    let boxtext = document.getElementsByClassName('boxtext');
     // total 9 Box text : [0,...,8]
 
-    let Wins=[
-        
-    //E[0],E[1],E[2]; 
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+    let Wins = [
+
+        //E[0],E[1],E[2]; 
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ];
 
 
@@ -52,18 +62,17 @@ const checkWin = () => {
     Wins.forEach((E) => {
 
         // Comparing all textbox with index Number
-      
-        if ((boxtext[E[0]].innerText===boxtext[E[1]].innerText) && (boxtext[E[1]].innerText===boxtext[E[2]].innerText) && (boxtext[E[0]].innerText!='')) 
-         {
+
+        if ((boxtext[E[0]].innerText === boxtext[E[1]].innerText) && (boxtext[E[1]].innerText === boxtext[E[2]].innerText) && (boxtext[E[0]].innerText != '')) {
             document.getElementById("Info").innerText = `${boxtext[E[1]].innerText} is Won `;
 
-             isGameOver=true;
+            isGameOver = true;
 
             //  displaynig a gif //
 
-            document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width='120px';
+            document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = '120px';
 
-         }
+        }
 
 
     });
@@ -97,26 +106,41 @@ Array.from(Boxes).forEach(element => {
 
     element.addEventListener('click', () => {
 
-        if (boxtext.innerText === '') {
+        if (boxtext.innerText === '')
+         {
             boxtext.innerText = Turn;
 
-            Turn = ChangeTurn();
+             Turn=ChangeTurn();
+
+
+                            // Change colour of turn //
+
+                            if (Turn=="X") {
+                                Player.style.color = "blue";
+                            }
+
+                            else {
+                                Player.style.color = "red";
+                            }
+
 
             audio_ting.play();
 
             checkWin();
 
 
-             if (isGameOver!=true) // work till game is Not Over
-            {               
-                document.getElementById("Info").innerText = `Turn for '${Turn}' `;
+            if (isGameOver != true) // work till game is Not Over
+            {
+                document.getElementById("playername").innerText = `${Turn}`;
             }
 
         }
 
 
-
+        
+        
+        
     });
-
+    
 });
 
